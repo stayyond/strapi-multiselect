7bb4af206b5a448f7c2d0ba1638c1f1c983e7bd7
Emily Vinsen <emilyv@flocc.co>
2022-11-09 15:21:45 +0000

    fixed admin index

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,40 +1,12 @@
 import { prefixPluginTranslations } from '@strapi/helper-plugin';
-import pluginPkg from '../../package.json';
+// import pluginPkg from '../../package.json';
 import pluginId from './pluginId';
-import Initializer from './components/Initializer';
 import MultiselectIcon from './components/Multiselect/MultiselectIcon';
 
-const name = pluginPkg.strapi.name;
+// const name = pluginPkg.strapi.name;
 
 export default {
   register(app) {
-    // app.addMenuLink({
-    //   to: `/plugins/${pluginId}`,
-    //   icon: PluginIcon,
-    //   intlLabel: {
-    //     id: `${pluginId}.plugin.name`,
-    //     defaultMessage: name,
-    //   },
-    //   Component: async () => {
-    //     const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
-
-    //     return component;
-    //   },
-    //   permissions: [
-    //     // Uncomment to set the permissions of the plugin here
-    //     // {
-    //     //   action: '', // the action name should be plugin::plugin-name.actionType
-    //     //   subject: null,
-    //     // },
-    //   ],
-    // });
-
-    app.registerPlugin({
-      id: pluginId,
-      initializer: Initializer,
-      isReady: false,
-      name,
-    });
     app.customFields.register({
         name: "multiselect",
         pluginId: "multiselect", // the custom field is created by a multiselect plugin
