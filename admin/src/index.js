b9c827d7f5240634350d6075c8a5f6d988ba0b72
Emily Vinsen <emily@yond.co>
2023-06-23 14:53:39 +0100

    added strapi api endpoint

--- admin/src/index.js
+++ admin/src/index.js
@@ -8,49 +8,49 @@ import MultiselectIcon from './components/Multiselect/MultiselectIcon';
 export default {
   register(app) {
     app.customFields.register({
-        name: "multiselect",
-        pluginId: "multiselect", // the custom field is created by a multiselect plugin
-        type: "json", // the selection will be stored as a json
-        intlLabel: {
-            id: "multiselect.multiselect.label",
-            defaultMessage: "Multiselect",
-        },
-        intlDescription: {
-            id: "multiselect.multiselect.description",
-            defaultMessage: "Select any items",
-        },
-        icon: MultiselectIcon, // don't forget to create/import your icon component 
-        components: {
-           Input: async () => import("./components/Multiselect/MultiselectInput"),
-        },
-        options: {
-            // declare options here
-          base: [
-              /*
-                Declare settings to be added to the "Base settings" section
-                of the field in the Content-Type Builder
-              */
-                {
-                    /*
-                      Add a "endpoint" url field
-                    */
-                    name: 'endpoint',
-                    intlLabel: {
-                        id: 'multiselect.multiselect.endpoint.label',
-                        defaultMessage: 'Endpoint',
-                    },
-                    intlDescription: {
-                        id: "multiselect.multiselect.endpoint.description",
-                        defaultMessage: "Enter the endpoint you wish to get the list of options from",
-                    },
-                    type: 'string',
-                },
-          ],
-        },
+      name: "multiselect",
+      pluginId: "multiselect", // the custom field is created by a multiselect plugin
+      type: "json", // the selection will be stored as a json
+      intlLabel: {
+        id: "multiselect.multiselect.label",
+        defaultMessage: "Multiselect",
+      },
+      intlDescription: {
+        id: "multiselect.multiselect.description",
+        defaultMessage: "Select any items",
+      },
+      icon: MultiselectIcon, // don't forget to create/import your icon component 
+      components: {
+        Input: async () => import("./components/Multiselect/MultiselectInput"),
+      },
+      options: {
+        // declare options here
+        base: [
+          /*
+            Declare settings to be added to the "Base settings" section
+            of the field in the Content-Type Builder
+          */
+          {
+            /*
+              Add a "endpoint" url field
+            */
+            name: 'endpoint',
+            intlLabel: {
+              id: 'multiselect.multiselect.endpoint.label',
+              defaultMessage: 'Endpoint Url',
+            },
+            intlDescription: {
+              id: "multiselect.multiselect.endpoint.description",
+              defaultMessage: "Enter the endpoint you wish to get the list of options from",
+            },
+            type: 'string',
+          },
+        ],
+      },
     });
   },
 
-  bootstrap(app) {},
+  bootstrap(app) { },
   async registerTrads({ locales }) {
     const importedTrads = await Promise.all(
       locales.map((locale) => {
